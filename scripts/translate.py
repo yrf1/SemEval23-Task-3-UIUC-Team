2dd2603b391574525a62c7e9c53b69469d5e24f2
Genglin Liu <genglinliu@gmail.com>
2023-01-23 15:30:58 -0600

    editing translation script

--- scripts/translate.py
+++ scripts/translate.py
@@ -10,41 +10,38 @@ data_dir = "/Users/genglinliu/Documents/GitHub/SemEval23-Task-3-UIUC-Team/data" 
 translator = Translator()
 
 mt_map = {"en":"en","fr":"fr","ge":"de","it":"it","po":"pl","ru":"ru"}
-
-# col_names=['doc_id', 'paragraph_id', 'text', 'extra_1', 'extra_2'] 
 col_names=['doc_id', 'paragraph_id', 'text']
-path = data_dir + "/po/train-labels-subtask-3.template"
-df = pd.read_csv(path, sep='\t', names=col_names, on_bad_lines="warn")
 
-for lang_dir in os.listdir(data_dir):
-    col_names=['doc_id', 'paragraph_id', 'text', 'extra_1', 'extra_2'] 
+target_lang = "en"
+
+# ex. we translate to English from the 5 other languages
+for src_lang in ["en", "fr", "ge", "it", "po", "ru"]:   
+
+    if target_lang == mt_map[src_lang]:
+        continue 
 
     for mode in ["train", "dev"]:
         # ex. "en/train-labels-subtask-3.template"
-        current_folder = lang_dir+"/"+mode+"-labels-subtask-3.template"
-        df = pd.read_csv(data_dir+"/"+current_folder, sep="\t", names=col_names, header=None)
+        current_folder = src_lang+"/"+mode+"-labels-subtask-3.template"
+        df = pd.read_csv(data_dir+"/"+current_folder, sep="\t", names=col_names, on_bad_lines="skip", header=None)
 
-        # ex. you're at English folder right now, so translate English to the 5 other languages
-        for target_lang in ["en", "fr", "de", "it", "pl", "ru"]:
+        print("current folder: " + current_folder)
+        print("Translating from: " + src_lang + " to: " + target_lang)
+        
+        column_to_translate = df["text"]
 
-            print("current folder: " + current_folder)
-            print("Translating from: " + lang_dir + " to: " + target_lang)
+        # create a new list to store the translated text
+        translated_column = []
 
-            if target_lang == mt_map[lang_dir]:
-                continue
-            
-            column_to_translate = df["text"]
+        # translate each element in the column
+        for text in tqdm(column_to_translate):
+            translated = translator.translate(text, dest=target_lang, src=mt_map[src_lang]).text #fr is the destination language
+            translated_column.append(translated)
 
-            # create a new list to store the translated text
-            translated_column = []
+        # add the translated column to the dataframe
+        df[src_lang+"-"+target_lang] = translated_column
 
-            # translate each element in the column
-            for text in tqdm(column_to_translate):
-                translated = translator.translate(text, dest=target_lang, src=mt_map[lang_dir]).text #fr is the destination language
-                translated_column.append(translated)
-
-            # add the translated column to the dataframe
-            df[str("translated_"+target_lang)] = translated_column
+        df = df[['doc_id', 'paragraph_id', target_lang]]
 
         # write the dataframe to a new CSV file
-        df.to_csv(data_dir+"/"+lang_dir+"/"+mode+"-translated.csv", sep="\t", index=False)
+        df.to_csv(src_lang+"-"+target_lang+"-translated.csv", sep="\t", index=False)
