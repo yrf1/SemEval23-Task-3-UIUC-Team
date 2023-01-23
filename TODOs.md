7f33fc5472bf03d900f3ef38edd4c40c88a6f4ed
genglinliu <genglinliu@gmail.com>
2023-01-23 00:46:36 -0600

    updating translate script and requirement, todos

--- TODOs.md
+++ TODOs.md
@@ -4,6 +4,19 @@ Deadline: submission deadline is 1/30
 
 Our focus is on subtask 3: persuasion techniques
 
+TODOs update 1/22
+ - Augment every language using EVERY other language
+
+```python
+languages_list = ["en", "fr", "ge", "it", "po", "ru"]
+for lang_1 in languages_list:
+    for lang_2 in languages_list:
+        if lang_1 != lang_2:
+            translate_1_to_2(src=lang_1, target=lang_2) # translate lang1 to lang2
+```
+
+ - Should also translate the label definitions to all 6 languages
+
 TODOs as of 1/19:
  - train on english dataset with 2021 task 6 data
  - restructure the code and make it modular
--- scripts/translate.py
+++ scripts/translate.py
@@ -1,25 +1,38 @@
 import pandas as pd
-from googletrans import Translator
+import os
+from tqdm import tqdm
+from googletrans import Translator # pip install googletrans==3.1.0a0
+from google_trans_new import google_translator
 
-# read in the CSV file
-df = pd.read_csv("file.csv")
+
+# data_dir = "data/"
+# for lang_dir in os.listdir(data_dir):
+#     labels = pd.read_csv(data_dir+"/"+lang_dir+"/"+"train-labels-subtask-3.template", sep="\t", header=None)
+
+# read in the tab separated template file
+col_names=['doc_id', 'paragraph_id', 'text'] 
+data_path = "/shared/nas/data/users/genglin2/SemEval/SemEval23-Task-3-UIUC-Team/data/en/dev-labels-subtask-3.template"
+df = pd.read_csv(data_path, sep='\t', names=col_names)
 
 # select the column you want to translate
-column_to_translate = df["column_name"]
+column_to_translate = df["text"]
 
 # initialize the translator
 translator = Translator()
+# translator = google_translator()
 
 # create a new list to store the translated text
 translated_column = []
 
 # translate each element in the column
-for text in column_to_translate:
-    translated = translator.translate(text, dest="fr").text #fr is the destination language
+for text in tqdm(column_to_translate):
+    translated = translator.translate(text, dest='fr', src='en').text #fr is the destination language
     translated_column.append(translated)
 
 # add the translated column to the dataframe
 df["translated_column_name"] = translated_column
+
+print(df)
 
 # write the dataframe to a new CSV file
 df.to_csv("translated_file.csv", index=False)
